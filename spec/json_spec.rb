4908fdd66c3687febd42abab8d9b42c2bd505f54
unknown <sshamim@spartaglobal.com>
2018-08-15 00:06:21 +0100

    all six tests now completed

--- spec/json_spec.rb
+++ spec/json_spec.rb
@@ -9,4 +9,26 @@ describe 'Create tests for exchange rates json' do
     expect(@a_test.json_file["base"]).to eq("EUR")
   end
 
+  it "should have a value EUR for key base" do
+    expect(@first_test.json_file["base"]).to eq("EUR")
+  end
+
+  it "should have a data type of floats for the currencies" do
+    expect(@first_test.json_file["date"]).to eq("2017-07-26")
+  end
+
+  it "should have a data type of floats for the currencies" do
+    expect(@first_test.json_file["rates"].values).to all(be_an(Float))
+  end
+  it "should have a data type of floats for the currencies" do
+    expect(@first_test.json_file["rates"].keys).to all(be_an(String))
+  end
+  it "should have a data type of floats for the currencies" do
+    expect(@first_test.json_file["rates"].length).to eq(31)
+  end
+  it "should have a very first key of Australian Dollar- AUD" do
+    expect(@first_test.json_file["rates"][0].key).to eq("AUD")
+  end
+
+
 end
